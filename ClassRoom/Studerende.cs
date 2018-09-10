c85efe3e2520ae04f18e3104a7281f11d9831c4d
Daniel <dadanner@gmail.com>
2018-09-10 18:03:55 +0200

    Lavet Studerende klassen.

---
+++ ClassRoom/Studerende.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class Studerende
+    {
+        private string _navn;
+        private int _fødselsmåned;
+        private int _fødselsdag;
+
+        public Studerende(string navn, int måned, int dag)
+        {
+            this._navn = navn;
+            this._fødselsmåned = måned;
+            this._fødselsdag = dag;
+        }
+
+        public string Navn
+        {
+            get { return _navn; }
+        }
+
+        public int Fødselsmåned
+        {
+            get { return _fødselsmåned; }
+        }
+
+        public int Fødselsdag
+        {
+            get { return _fødselsdag; }
+        }
+    }
+}
