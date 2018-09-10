9102afa8fc5a15c13f480d3c47d48326a6a79fe7
Daniel <dadanner@gmail.com>
2018-09-10 18:21:12 +0200

    Tilføjet KlasseRum klassen.

---
+++ ClassRoom/KlasseRum.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class KlasseRum
+    {
+        private string _klassenavn;
+        private List<Studerende> _klasseliste;
+        private DateTime _semesterstart;
+
+        public KlasseRum(string klassenavn, List<Studerende> klasseliste, DateTime semesterstart)
+        {
+            this._klassenavn = klassenavn;
+            this._klasseliste = klasseliste;
+            this._semesterstart = semesterstart;
+        }
+
+        public string Klassenavn
+        {
+            get { return _klassenavn; }
+        }
+
+        public List<Studerende> Klasseliste
+        {
+            get { return _klasseliste; }
+        }
+
+        public DateTime Semesterstart
+        {
+            get { return _semesterstart; }
+        }
+    }
+}
